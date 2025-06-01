16935210482458d5033a34bd8d00048e736bc0dd
giangnd99 <nguyendanggiang99@gmail.com>
2025-06-01 14:01:22 +0700

    architecture: 3-tier - room management

---
+++ spring-ai-management/ai-dao/src/main/java/com/poly/ai/management/dao/booking/feign/BookingFeignImpl.java
@@ -0,0 +1,7 @@
+package com.poly.ai.management.dao.booking.feign;
+
+import com.poly.ai.management.domain.port.output.feign.BookingFeign;
+
+public class BookingFeignImpl implements BookingFeign {
+
+}
