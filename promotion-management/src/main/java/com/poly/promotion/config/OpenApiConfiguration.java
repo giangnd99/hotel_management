46ef23b669a7f6402f5de77bf206194c47ef7f12
LinhPete <peterdamlinh1215@gmail.com>
2025-06-07 10:35:18 +0700

    add spring doc open api

---
+++ promotion-management/src/main/java/com/poly/promotion/config/OpenApiConfiguration.java
@@ -0,0 +1,32 @@
+package com.poly.promotion.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfiguration {
+    @Bean
+    public OpenAPI defineOpenApi() {
+        Server server = new Server();
+        server.setUrl("http://localhost:8088/promotion-management");
+        server.setDescription("Development");
+
+        Contact myContact = new Contact();
+        myContact.setName("Jane Doe");
+        myContact.setEmail("your.email@gmail.com");
+
+        Info information = new Info()
+                .title("Promotion Management System API")
+                .version("1.0")
+                .description("This API exposes endpoints to manage promotions.")
+                .contact(myContact);
+        return new OpenAPI().info(information).servers(List.of(server));
+    }
+
+}
