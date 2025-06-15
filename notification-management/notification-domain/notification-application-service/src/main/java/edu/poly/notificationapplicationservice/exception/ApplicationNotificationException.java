0ac1bea0ee625cb9ed450425af0cf68cb0552309
TaiNguyen1103 <tai30799@gmail.com>
2025-06-15 19:10:28 +0700

    Revert "Feature/notification management"

--- notification-management/notification-domain/notification-application-service/src/main/java/edu/poly/notificationapplicationservice/exception/ApplicationNotificationException.java
+++
@@ -1,7 +0,0 @@
-package edu.poly.notificationapplicationservice.exception;
-
-public class ApplicationNotificationException extends Exception {
-    public ApplicationNotificationException(String message) {
-        super(message);
-    }
-}
