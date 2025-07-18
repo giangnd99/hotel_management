07ca2fe1c8a7c20de2a96819c84660e849d0de97
giangnd99 <nguyendanggiang99@gmail.com>
2025-07-18 14:33:01 +0700

    build domain0-core: service, restaurant

--- booking-management/booking-domain/booking-application-service/src/main/java/com/poly/booking/management/domain/dto/BookingDto.java
+++ booking-management/booking-domain/booking-application-service/src/main/java/com/poly/booking/management/domain/dto/BookingDto.java
@@ -1,6 +1,6 @@
 package com.poly.booking.management.domain.dto;
 
-import com.poly.domain.valueobject.BookingStatus;
+import com.poly.domain.valueobject.EBookingStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -26,6 +26,6 @@ public class BookingDto {
     private int numberOfGuests;
     private BigDecimal totalAmount;
     private LocalDateTime bookingDate;
-    private BookingStatus status;
+    private EBookingStatus status;
     private String specialRequests;
 }
