10781353d5e4a03979b7c6c7aea22d6ba6108bd3
Hùng Phan <112998342+lamhungphan@users.noreply.github.com>
2025-08-01 13:05:23 +0700

    handle more logic in handler impl

--- restaurant-management/restaurant-data-access/src/main/java/com/poly/restaurant/dataaccess/jpa/JpaOrderRepository.java
+++ restaurant-management/restaurant-data-access/src/main/java/com/poly/restaurant/dataaccess/jpa/JpaOrderRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JpaOrderRepository extends JpaRepository<OrderJpaEntity, Integer> {
+public interface JpaOrderRepository extends JpaRepository<OrderJpaEntity, String> {
 }
