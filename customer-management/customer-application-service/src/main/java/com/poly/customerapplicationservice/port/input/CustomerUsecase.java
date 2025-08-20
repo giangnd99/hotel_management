75a27ec57248e352e2beb37d6886f2acb3539ac6
giangnd99 <nguyendanggiang99@gmail.com>
2025-08-20 13:07:03 +0700

    fix conflixt

--- customer-management/customer-application-service/src/main/java/com/poly/customerapplicationservice/port/input/CustomerUsecase.java
+++ customer-management/customer-application-service/src/main/java/com/poly/customerapplicationservice/port/input/CustomerUsecase.java
@@ -14,9 +14,6 @@ public interface CustomerUsecase {
     CustomerDto retrieveCustomerProfileById(RetrieveCustomerProfileCommand command);
     PageResult<CustomerDto> retrieveAllCustomers(int page, int size);
     CustomerDto ChangeCustomerInformation (UpdateCustomerCommand command);
-<<<<<<< HEAD
     CustomerDto findCustomerById(UUID customerId);
-=======
     CustomerDto updateCustomerAvatar(String customerId, String imageLink);
->>>>>>> 9e5c2efb57e81c48ff4e55f850db5a31a866592d
 }
