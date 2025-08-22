88e00a837cf21bff2629cdf9f6e0b91826c4dc23
quytt111 <tai30799@gmail.com>
2025-08-22 12:55:54 +0700

    pull code moi

--- api-gateway/authentication-service/authentication-container/src/main/java/com/poly/authentication/service/domain/AuthenticationApplication.java
+++ api-gateway/authentication-service/authentication-container/src/main/java/com/poly/authentication/service/domain/AuthenticationApplication.java
@@ -4,13 +4,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
-@EntityScan(basePackages = {"com.poly.authentication.service.dao"})
+@EntityScan(basePackages = {"com.poly"})
 @EnableJpaRepositories(basePackages = {"com.poly.authentication.service.dao"})
 @EnableRedisRepositories(basePackages = {"com.poly.authentication.service.dao.token.repository"})
 @SpringBootApplication(scanBasePackages = "com.poly")
+@ComponentScan(value = {"com.poly"})
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = {"com.poly.authentication.service.domain.port"})
 public class AuthenticationApplication {
